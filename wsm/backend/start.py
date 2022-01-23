4b5c994cc5f846823208817495050597a2943be9
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-23 18:10:45 +0800

    Add start

---
+++ wsm/backend/start.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import Callable, Dict, List
+from .utils import parse_datetime
+from .services import create_failed, create_success
+from .followers import AcceptedOrFailedStatusFollower, BaseStatusFollower
+
+status_factory: Dict[str, Callable[[str, str, datetime], List]] = {
+    "Failed": create_failed,
+    "Accepted": create_success,
+}
+
+
+def start(file_path) -> None:
+    follower: BaseStatusFollower = AcceptedOrFailedStatusFollower(file_path)
+    for line in follower:
+        line: str = list(line)[0]
+        create: Callable[[str, str, datetime], List] = status_factory[line["status"]]
+        utc: datetime = parse_datetime(line["date"], line["time"])
+        create(line["ip_address"], line["username"], utc)
