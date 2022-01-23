7c01d15c48355c2214a19fd226cb3c8dfda2d321
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-23 18:35:17 +0800

    add get_to_ban

--- wsm/backend/services/__init__.py
+++ wsm/backend/services/__init__.py
@@ -1,5 +1,5 @@
 from .allow import create_allow
 from .banned import create_banned
 from .deny import create_deny
-from .failed import create_failed, get_failed
+from .failed import create_failed, get_failed, get_to_ban
 from .success import create_success
--- wsm/backend/services/failed.py
+++ wsm/backend/services/failed.py
@@ -10,3 +10,20 @@ def create_failed(ip: str, username: str, time: datetime) -> List:
 
 def get_failed() -> List:
     return query("SELECT * FROM failed")
+
+
+def get_to_ban(start_time: datetime, end_time: datetime, max_retry: int) -> List:
+    to_ban: str = """
+    SELECT ip
+    FROM (
+        SELECT ip, count(ip) AS count
+        FROM failed
+        WHERE time BETWEEN :start_time AND :end_time
+        GROUP BY ip
+    ) 
+    WHERE count > :max_retry
+    """
+    result: List = query(
+        to_ban, {"start_time": start_time, "end_time": end_time, "max_retry": max_retry}
+    )
+    return list(map(lambda x: x[0], result))
