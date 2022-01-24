d2a67b628ea976e99f1036a077e74fc34d36fbc4
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-24 20:32:43 +0800

    Add a few banned services

--- wsm/backend/services/__init__.py
+++ wsm/backend/services/__init__.py
@@ -1,5 +1,11 @@
 from .allow import create_allow
-from .banned import create_banned
+from .banned import (
+    create_banned,
+    create_many_banned,
+    delete_expired,
+    get_banned_ips,
+    delete_expired,
+)
 from .deny import create_deny
 from .failed import create_failed, get_failed, get_to_ban
 from .success import create_success
--- wsm/backend/services/banned.py
+++ wsm/backend/services/banned.py
@@ -1,6 +1,21 @@
-from ._types import datetime, List
+from ._types import datetime, List, Iterable, Tuple
 from ..models.model import query
+from ..utils import generate_expire
 
 
 def create_banned(ip: str, expire: datetime) -> List:
     return query("INSERT INTO banned (ip, expire) VALUES (?, ?)", (ip, expire))
+
+
+def create_many_banned(iterable: Iterable[Tuple[str, int]]) -> List:
+    return query(
+        "INSERT INTO banned (ip, expire) VALUES (?, ?)", params=iterable, mode="many"
+    )
+
+
+def get_banned_ips() -> List:
+    return query("SELECT ip FROM banned", row_factory=lambda cursor, row: row[0])
+
+
+def delete_expired() -> List:
+    return query("DELETE FROM banned WHERE expire < ?", (generate_expire(0),))
