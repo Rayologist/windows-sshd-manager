c9759f495bcd1fdffe64429045064573a5551f19
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-31 03:49:08 +0800

    Add fake powershell

--- wsm/backend/powershell.py
+++ wsm/backend/powershell.py
@@ -3,7 +3,7 @@ from functools import wraps
 from typing import Optional, Iterable
 
 
-class PowerShell:
+class PowerShell1:
     def __init__(self):
         self.name: str = "wsm"
 
@@ -41,3 +41,38 @@ class PowerShell:
             f"if (-not $exist) {{New-NetFirewallRule -DisplayName {self.name} -Direction Inbound -Action Block -LocalAddress '0.0.0.0' -RemoteAddress '0.0.0.0'}}"
         )
         return self.run(script)
+
+class PowerShell:
+    def __init__(self):
+        self.name: str = "wsm"
+        self.firewall= "/Users/rayologist/Desktop/wsm/firewall.txt"
+
+    def block_ips(self, ips: Iterable[str]):
+        concat_ips: str = ", ".join(map(lambda x: f"'{x}'", ips))
+        with open(self.firewall, "w") as f:
+            processed = concat_ips.replace("'", "").replace(", ", "\n")
+            f.write(processed)
+
+        return 
+
+    def set_execution_policy(self):
+        # script: str = (
+        #     f"if ((Get-ExecutionPolicy) -ne 'Unrestricted')"
+        #     f"{{Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}}"
+        # )
+        print("Set execution policy")
+        return 
+
+    def get_firewall_content(self):
+        with open(self.firewall, 'r') as f:
+            content = f.read()
+        return content
+        # return self.run(script)
+
+    def set_new_firewall_rule(self):
+        # script: str = (
+        #     f"$exist = (Get-NetFirewallRule).DisplayName.Contains('{self.name}');"
+        #     f"if (-not $exist) {{New-NetFirewallRule -DisplayName {self.name} -Direction Inbound -Action Block -LocalAddress '0.0.0.0' -RemoteAddress '0.0.0.0'}}"
+        # )
+        print("Set firewall")
+        # return self.run(script)
