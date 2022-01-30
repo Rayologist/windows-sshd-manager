1a901be6932adf041799ca3c22cbaedd769b5963
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-31 03:50:34 +0800

    Add deny services

--- wsm/backend/services/deny.py
+++ wsm/backend/services/deny.py
@@ -1,6 +1,18 @@
 from ._types import List
 from ..models.model import query
+from datetime import datetime
 
 
 def create_deny(ip: str) -> List:
-    return query("INSERT INTO deny (ip) VALUES (?)", (ip,))
+    query(
+        "INSERT INTO banned (ip, expire) VALUES (?, ?)",
+        (ip, datetime(year=9999, month=12, day=31)),
+    )
+    query("INSERT INTO deny (ip) VALUES (?)", (ip,))
+    return []
+
+
+def delete_deny(ip: str) -> List:
+    query("delete from banned where ip=?", (ip,))
+    query("delete from deny where ip=?", (ip,))
+    return []
