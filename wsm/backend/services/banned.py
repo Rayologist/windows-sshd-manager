4b95a3fda661f2b05cedf728fb50bd1a28b60c3f
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-31 03:50:18 +0800

    Format and Add row factory

--- wsm/backend/services/banned.py
+++ wsm/backend/services/banned.py
@@ -9,7 +9,9 @@ def create_banned(ip: str, expire: datetime) -> List:
 
 def create_many_banned(iterable: Iterable[Tuple[str, int]]) -> List:
     return query(
-        "INSERT INTO banned (ip, expire) VALUES (?, ?)", params=iterable, mode="many"
+        "INSERT INTO banned (ip, expire) VALUES (?, ?) ON CONFLICT DO NOTHING",
+        params=iterable,
+        mode="many",
     )
 
 
@@ -18,4 +20,8 @@ def get_banned_ips() -> List:
 
 
 def delete_expired() -> List:
-    return query("DELETE FROM banned WHERE expire < ?", (generate_expire(0),))
+    return query(
+        "DELETE FROM banned WHERE expire < ? RETURNING ip",
+        (generate_expire(0),),
+        row_factory=lambda cursor, row: row[0],
+    )
