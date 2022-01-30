484e4ca39b17f0fcfd42528bec9938a26fbafd9e
Bo-wei Chen <rayologist1002@gmail.com>
2022-01-31 03:49:40 +0800

    Add get_allow

--- wsm/backend/services/allow.py
+++ wsm/backend/services/allow.py
@@ -4,3 +4,7 @@ from ..models.model import query
 
 def create_allow(ip: str) -> List:
     return query("INSERT INTO allow (ip) VALUES (?)", (ip,))
+
+
+def get_allow() -> List:
+    return query("SELECT ip FROM allow", row_factory=lambda cursor, row: row[0])
