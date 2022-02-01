2304c13ca9eac8edbf04c4822795cccbda60f5c9
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-01 22:45:52 +0800

    Import async_query

--- wsm/backend/__init__.py
+++ wsm/backend/__init__.py
@@ -1,5 +1,5 @@
 from .config import check_config, CONFIG_PATH, update_config, DB_PATH, get_config
-from .models.model import query, init_db
+from .models.model import query, init_db, async_query
 from .followers import *
 from .services import *
 from .utils import parse_datetime, generate_expire, is_ipv4_address
