20ee7228fc82562b1885c7a4b5bbd10457a8e486
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-03 01:03:59 +0800

    Update stats display style

--- wsm/backend/controllers/stats.py
+++ wsm/backend/controllers/stats.py
@@ -11,13 +11,13 @@ async def report_stats(log_path, find_time):
     ) = await get_stats(find_time)
 
     reports = (
-        f"Failed:\n"
-        f"  Currently failed: {currently_failed[0]}\n"
-        f"  Total failed: {total_failed[0]}\n"
-        f"  Watching file: {log_path}\n"
-        f"Banned:\n"
-        f"  Currently banned: {currently_banned[0]}\n"
-        f"  Total banned: {total_banned[0]}\n"
-        f"""  Current banned ips: {", ".join(get_banned_ips)}\n"""
+        f"- Failed:\n"
+        f"  * Currently failed: {currently_failed[0]}\n"
+        f"  * Total failed: {total_failed[0]}\n"
+        f"  * Log file path: {log_path}\n"
+        f"- Banned:\n"
+        f"  * Currently banned: {currently_banned[0]}\n"
+        f"  * Total banned: {total_banned[0]}\n"
+        f"""  * Banned IP list: {", ".join(get_banned_ips)}\n"""
     )
     return reports
