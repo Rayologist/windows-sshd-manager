47916264b540c53e103ae39b8449188d1c96e804
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-04 00:42:05 +0800

    Add new crud operation and refactor

--- wsm/backend/services/deny.py
+++ wsm/backend/services/deny.py
@@ -1,18 +1,34 @@
+from typing import Iterable
 from ._types import List
 from ..models.model import query
 from datetime import datetime
+from ..utils import generate_expire
 
 
-def create_deny(ip: str) -> List:
+def get_deny() -> List:
+    return query("SELECT ip FROM DENY", row_factory=lambda cursor, row: row[0])
+
+
+def create_deny(ips: List) -> List:
     query(
         "INSERT INTO banned (ip, expire) VALUES (?, ?)",
-        (ip, datetime(year=9999, month=12, day=31)),
+        [(ip, datetime(year=9999, month=12, day=31)) for ip in ips],
+        mode="many",
     )
-    query("INSERT INTO deny (ip) VALUES (?)", (ip,))
+    query("INSERT INTO deny (ip) VALUES (?)", (ips,), mode="many")
     return []
 
 
-def delete_deny(ip: str) -> List:
-    query("delete from banned where ip=?", (ip,))
-    query("delete from deny where ip=?", (ip,))
+def delete_deny(ips: List) -> List:
+    query(
+        """
+        UPDATE banned 
+        SET expire = :now 
+        WHERE ip=:ip 
+        AND expire > :now
+        """,
+        [{"ip": ip, "now": generate_expire(0)} for ip in ips],
+        mode="many",
+    )
+    query("delete from deny where ip=?", (ips,), mode="many")
     return []
