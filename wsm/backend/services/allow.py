ea69d50e17ce3ee5b16db35c34ac89f31923625a
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-04 00:41:53 +0800

    Add new crud operation

--- wsm/backend/services/allow.py
+++ wsm/backend/services/allow.py
@@ -1,10 +1,26 @@
 from ._types import List
 from ..models.model import query
+from ..utils import generate_expire
 
 
-def create_allow(ip: str) -> List:
-    return query("INSERT INTO allow (ip) VALUES (?)", (ip,))
+def create_allow(ips: List) -> List:
+    query(
+        """
+        UPDATE banned
+        SET expire = :now
+        WHERE expire > :now
+        AND ip = :ip
+        """,
+        [{"now": generate_expire(0), "ip": ip} for ip in ips],
+        mode="many",
+    )
+
+    return query("INSERT INTO allow (ip) VALUES (?)", (list(ips),), mode="many")
 
 
 def get_allow() -> List:
     return query("SELECT ip FROM allow", row_factory=lambda cursor, row: row[0])
+
+
+def delete_allow(ips: List) -> List:
+    return query("DELETE FROM allow WHERE ip = ?", (ips,), mode="many")
