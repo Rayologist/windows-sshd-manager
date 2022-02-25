b0679a1f2122b95c9073475e31e7b4b0d2299241
Bo-wei Chen <rayologist1002@gmail.com>
2022-02-25 21:17:43 +0800

    Update type hints

--- wsm/backend/controllers/allow.py
+++ wsm/backend/controllers/allow.py
@@ -1,25 +1,25 @@
-from typing import List
+from typing import List, Optional, Set
 from ..services import get_deny, create_allow, delete_allow, get_allow
 from ..utils import is_ipv4_address
 
 
-def allow(ips: List):
-    invalid_ips = list(filter(lambda x: not is_ipv4_address(x), ips))
+def allow(ips: List) -> Optional[List]:
+    invalid_ips: List = list(filter(lambda x: not is_ipv4_address(x), ips))
     if invalid_ips:
         print(f"Invalid ip address(es): {', '.join(invalid_ips)} ")
         return
-    ips = set(ips)
-    denied = set(get_deny())
+    ips: Set = set(ips)
+    denied: Set = set(get_deny())
     if ips & denied:
         print(
             "Plese lift deny before allowing the following: ", ", ".join(ips & denied)
         )
-    currently_allowed = set(get_allow())
+    currently_allowed: Set = set(get_allow())
     to_allow: List = list(ips - denied - currently_allowed)
 
     if to_allow:
         return create_allow(to_allow)
 
 
-def lift_allow(ips: List):
+def lift_allow(ips: List) -> List:
     return delete_allow(ips)
